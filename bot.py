4e2fc7422e8f01a7003244d98b34a4802a34e839
Jin Yi <jinyi2001.jl@gmail.com>
2019-06-24 20:28:49 -0700

    use discord.ext tasks

--- bot.py
+++ bot.py
@@ -3,13 +3,21 @@ import time
 from datetime import datetime
 import requests
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-bot = commands.Bot(command_prefix=os.environ["cmd_prefix"])
-monitoring = False
+CMD_PREFIX = os.environ["cmd_prefix"]
+OFFLINE_DELAY = os.environ["off_delay"]
+ONLINE_DELAY = os.environ["on_delay"]
+TOKEN = os.environ["token"]
 
 
-def create_embed(title, message):
+bot = commands.Bot(command_prefix=CMD_PREFIX)
+
+state = ""
+monitor_channel = None
+
+
+async def create_embed(title, message):
     embed = discord.Embed(
         title=title,
         type="rich",
@@ -21,12 +29,23 @@ def create_embed(title, message):
     return embed
 
 
-@bot.event
-async def on_ready():
+async def set_presence(message):
     await bot.change_presence(activity=discord.Activity(
-        name="butterflies",
+        name=message,
         type=discord.ActivityType.watching,
         start=datetime.utcnow()))
+
+
+@bot.event
+async def on_ready():
+    global state
+    # get current state
+    r = requests.get("https://status.pbe.leagueoflegends.com/shards/pbe")
+    state = r.json()["services"][0]["status"]
+
+    # set presence to show pbe server status
+    await set_presence(f"PBE | {CMD_PREFIX}start")
+
     print(bot.user.name + " Launched")
     print("By Jin Yi")
     print("-" * 20)
@@ -34,32 +53,62 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send(embed=create_embed("PBE Monitor", "Pong!"))
+    print(monitor.current_loop)
+    await ctx.send(embed=await create_embed("PBE Monitor", "Pong!"))
 
 
 @bot.command()
 async def start(ctx):
-    global monitoring
-    monitoring = True
-    # update presence to indicate monitor is on
-    await bot.change_presence(activity=discord.Activity(
-        name="the PBE server status",
-        type=discord.ActivityType.watching,
-        start=datetime.utcnow()))
-    while monitoring:
-        r = requests.get("https://status.pbe.leagueoflegends.com/shards/pbe")
-        if r.status_code == 200 and r.json()["services"][0]["status"] != "offline":
-            await ctx.send(embed=create_embed("PBE LIVE!", "Sign in quickly!"))
-        time.sleep(int(os.environ["delay"]))
+    global state, monitor_channel
+    # check if it is on a loop
+    if monitor.current_loop:
+        await ctx.send(embed=await create_embed("PBE Monitor", "Bot is already monitoring"))
+        return
+
+    monitor_channel = ctx.channel
+    await ctx.send(embed=await create_embed("PBE Monitor", "Bot is now watching the PBE server status"))
+    await set_presence(f"PBE | Status: {state.upper()}")
+    await monitor.start()
 
 
 @bot.command()
 async def stop(ctx):
-    global monitoring
-    monitoring = False
-    await bot.change_presence(activity=discord.Activity(
-        name="butterflies",
-        type=discord.ActivityType.watching,
-        start=datetime.utcnow()))
+    if monitor.current_loop == 0:
+        await ctx.send(embed=await create_embed("PBE Monitor", "Bot is already stopped"))
+        return
 
-bot.run(os.environ["token"])
+    monitor.stop()
+    await ctx.send(embed=await create_embed("PBE Monitor", "Bot stopped monitoring"))
+    await set_presence(f"PBE | {CMD_PREFIX}start")
+
+
+@tasks.loop(seconds=10)
+async def monitor():
+    global state, monitor_channel
+    # get server status
+    r = requests.get("https://status.pbe.leagueoflegends.com/shards/pbe")
+    new_state = r.json()["services"][0]["status"]
+
+    if r.status_code == 200:
+        if new_state != state:
+            state = new_state
+            if state == "online":
+                await monitor_channel.send(embed=await create_embed("PBE LIVE!", "Sign in quickly!"))
+                await set_presence(f"PBE | Status: {state.upper()}")
+
+            else:
+                await monitor_channel.send(embed=await create_embed("PBE Server Update", "Server is offline"))
+                await set_presence(f"PBE | Status: {state.upper()}")
+
+        # when server is online, long delay, offline short delay
+        if state == "online":
+            monitor.change_interval(seconds=int(ONLINE_DELAY))
+        else:
+            monitor.change_interval(seconds=int(OFFLINE_DELAY))
+
+    # sleep, status_code != 200 probably is a ban
+    else:
+        monitor.change_interval(seconds=10)
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
